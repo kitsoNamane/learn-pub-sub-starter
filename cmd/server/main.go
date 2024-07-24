be9aef38e24802a8c578f75f86ad250c0fa9f30d
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-07-24 12:16:17 +0200

    feat(amqp): setup amqp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
 	fmt.Println("Starting Peril server...")
+	connUrl := "amqp://guest:guest@0.0.0.0:5672/"
+	conn, err := amqp091.Dial(connUrl)
+	if err != nil {
+		slog.Error("failed to connect to rabbitmq server", slog.String("error_msg", err.Error()))
+		os.Exit(-1)
+	}
+	defer conn.Close()
+	fmt.Println("connection successful")
+
+	<-signalChan
 }
