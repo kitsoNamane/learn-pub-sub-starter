202c7c7230d99247c5205c67f08ea1597c8f8c63
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-07-31 20:37:00 +0200

    feat: transient queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	connUrl := "amqp://guest:guest@0.0.0.0:5672/"
+	conn, err := amqp091.Dial(connUrl)
+	if err != nil {
+		slog.Error("failed to connect to rabbitmq server", slog.String("error_msg", err.Error()))
+		os.Exit(-1)
+	}
+	defer conn.Close()
+	fmt.Println("connection successful")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		slog.Error("failed to get customer name", slog.String("error_msg", err.Error()))
+		os.Exit(-1)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("pause.%s", name), routing.PauseKey, 0)
+	if err != nil {
+		slog.Error("failed to create and bind a queue", slog.String("error_msg", err.Error()))
+		os.Exit(-1)
+	}
+
+	<-signalChan
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -19,3 +19,37 @@ func PublishJSON[T any](ch *amqp091.Channel, exchange, key string, val T) error 
 		amqp091.Publishing{ContentType: "application/json", Body: valJSON})
 	return nil
 }
+
+func DeclareAndBind(conn *amqp091.Connection, exchange, queueName, key string, simpleQueueType int) (*amqp091.Channel, amqp091.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		slog.Error("failed to create channel", slog.String("error_msg", err.Error()))
+		return nil, amqp091.Queue{}, err
+	}
+	defer ch.Close()
+
+	var durable, autoDelete, exclusive bool
+	if simpleQueueType == 0 {
+		durable = false
+		autoDelete = true
+		exclusive = true
+	} else {
+		durable = true
+		autoDelete = false
+		exclusive = false
+	}
+
+	queue, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		slog.Error("failed to declare a queue", slog.String("error_msg", err.Error()))
+		return nil, amqp091.Queue{}, err
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		slog.Error("failed to bind queue to exchange", slog.String("error_msg", err.Error()))
+		return nil, amqp091.Queue{}, err
+	}
+
+	return ch, queue, nil
+}
